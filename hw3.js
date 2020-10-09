ebd8c99ed438cf24edd83151d399a47396cb1b8d
Gina <e.kurmeneva@gmail.com>
2020-10-09 17:03:00 +0300

    Complete task 1
    (cherry picked from commit 9ae5f88b97ff841d32a2a6ded1d89c53c203425f)

--- hw3.js
+++ hw3.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Task 1
+function splitAndMerge(str, sp) {
+  var array = str.split(" ").map(function (elem) {
+    return elem.split("").join(sp);
+  })
+  return array.join(" ");
+}
+
+// console.log(splitAndMerge("My name is John"," "));
